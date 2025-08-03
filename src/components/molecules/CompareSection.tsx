4d1b238d32845acd8a04784eb85a67d372f60b25
quiniapp <quini.app.leo@gmail.com>
2025-08-03 14:06:25 -0300

    suspense  compare setcion

--- src/components/molecules/CompareSection.tsx
+++ src/components/molecules/CompareSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { Suspense } from "react";
 import Flex from "../atoms/Flex";
 import CompareVehicleCard from "./CompareVehicleCard";
 import { useFakeFetch } from "@/helper/functions/fakeFetch";
@@ -12,27 +12,24 @@ const CompareSection = () => {
   const count = useCardCountByBreakpoint();
   const { selections, updateSelection } = useCompareSelections(count);
 
-
   if (!data) return null;
 
-  
   return (
     <Flex className="gap-8 flex-wrap items-start">
       {Array.from({ length: count }).map((_, index) => {
-        
-            
         const selectedIds = selections.filter((_, i) => i !== index); // exclude current
         const options = data?.filter(
           (vehicle) => !selectedIds.includes(vehicle.id)
         );
 
         return (
-          <CompareVehicleCard
-            key={index}
-            selectOptions={options}
-            value={selections[index] ?? null}
-            onChange={(value) => updateSelection(index, value)}
-          />
+          <Suspense fallback={<>Cargando</>} key={index}>
+            <CompareVehicleCard
+              selectOptions={options}
+              value={selections[index] ?? null}
+              onChange={(value) => updateSelection(index, value)}
+            />
+          </Suspense>
         );
       })}
     </Flex>
